06d355102f8a9ea8242da33f0e496bb80a8111f8
Afzal Shadab <mashadab@mit.edu>
2021-06-11 22:49:09 -0500

    Working analysis code

--- video_analysis.m
+++ video_analysis.m
@@ -18,20 +18,17 @@ data = []; %the data will be compiled here
 fps = 60; %frames rate (per second)
 threshold = 70; %setting the threshold of the image
 
-%fprintf("Hi");
 
-%error('Breaking out of function1');
-
-
+%Manually setting the region of importance
 top_height = 321;
 bottom_height = 543;
 x_origin = 96;
 x_right  = 1243;
 
 crop_drop=  [x_origin top_height (x_right - x_origin) (bottom_height - top_height)]; %cropping the drop region: left, top, width, height
-frame_begin = 3000;  %starting frame
-frame_end = 4000;   %end frame
-frame_skip = 10;
+frame_begin = 790;  %starting frame
+frame_end = 30790;   %end frame
+frame_skip = 60;
 position_timer = [0, 50];   %position of the timer
 
 % Read the video in a standard MATLAB color video format
@@ -56,9 +53,13 @@ vid.NumberOfFrames
 
 rgbImage1 = read(vid,1);
 rgbImage = imcrop(rgbImage1,crop_drop);
-grayImg_main  = rgb2gray(rgbImage);
+grayImg_main = rgb2gray(rgbImage);
+
+height = zeros(floor((frame_end-frame_begin)/frame_skip),size(grayImg_main,2));
+time   = zeros(floor((frame_end-frame_begin)/frame_skip),1);
 
 %starting looping over frames for analysis
+ii = 1;
  for frame = frame_begin:frame_skip:frame_end %vid.NumberOfFrames;
 all = [0 0 0 0]; %initializing the matrix with all the data
 rgbImage1 = read(vid,frame);
@@ -136,6 +137,8 @@ while n < size(bwImage,2)+1
     
 end
 
+height(ii,:) = Ib - Ii;
+time(ii) = (frame-frame_begin)/fps;
 
 %// Step #3 Find regions of drops
 %rp = regionprops(im_thresh, 'BoundingBox', 'Area');
@@ -154,6 +157,9 @@ set(gcf, 'units','normalized','outerposition',[0, 0, 1, 1]);
 F(frame) = getframe(gcf) ;
 hold off
 drawnow
+
+
+ii = ii+1;
  end
 
 % create the video writer with fps of the original video
@@ -172,175 +178,6 @@ end
 % close the writer object
 close(writerObj);
 close all
- 
- 
- 
-%{
-%// Step #3 Find regions of drops
-rp = regionprops(im_thresh, 'BoundingBox', 'Area');
 
-%if no drop found
-if isempty(rp) %& frame ~= frame_end
-    %frame = frame +1 
-figure('Visible','Off')
-% Enlarge figure to full screen.
-set(gcf, 'units','normalized','outerposition',[0, 0, 1, 1]);
-subplot(2,2,[1 3])
-text_str = ['Time: ' num2str((frame-frame_begin)/fps,'%0.2f') 's'];
-timer_img = insertText(rgbImage1,position_timer,text_str,'FontSize',100,'BoxOpacity',0.0,'TextColor','black');
-imshow(timer_img)
-% Enlarge figure to full screen.
-set(gcf, 'units','normalized','outerposition',[0, 0, 1, 1]);
-F(frame) = getframe(gcf) ;
-hold on
-
-subplot(2,2,2)
-yyaxis left
-loglog(data(:,1),data(:,8),'^')
-xlim([0,(frame_end-frame_begin)/fps])
-ylim([0,600])
-ylabel('$$Height\quad(mm)$$','Interpreter','latex')
-xlabel('$$Time\quad (s)$$','Interpreter','latex')
-hold on
-yyaxis right
-loglog(data(:,1),data(:,9),'o')
- xlim([0,(frame_end-frame_begin)/fps])
-%ylim([0,200])
- ylabel('$$Velocity\quad(mm/s)$$','Interpreter','latex')
- xlabel('$$Time\quad (s)$$','Interpreter','latex')
- hold on
-F(frame) = getframe(gcf) ;
-
- subplot(2,2,4)
- semilogx(data(:,1),data(:,6),'^')
-%semilogx(data(:,1),data(:,6)./data(:,7),'^')
-xlim([0,(frame_end-frame_begin)/fps])
-%ylim([0,3])
-ylabel('$$Width \quad (mm)$$','Interpreter','latex')
-xlabel('$$Time\quad (s)$$','Interpreter','latex')
-hold on
-F(frame) = getframe(gcf) ;
-drawnow
-hold off
-
-    data= [data;(frame-frame_begin)/fps,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN,NaN]; %Time (s), ruler reading (mm), reference pixel,bounding box - left pixel, top pixel, width, height 
-    continue
-end
-
-%// Step #4 Calculating the parameters and plotting regions on the image
-area = [rp.Area].';
-[~,ind] = max(area);
-bb = rp(ind).BoundingBox;
-drop_end=bb(2)+bb(4);
-
-if bb(4)>3.0*bb(3) %if height is greater than 3 times the width
-    drop_top = drop_end-3.0*bb(3);
-    bb(2) = drop_top; 
-    bb(4)=3.0*bb(3);
-end
-
-area_drop=imcrop(im_thresh,bb);
-area_drop=S2*S2*bwarea(area_drop); %area in mm^2
-
-% Display the original color image.
-boxRectruler = [all(3)+crop_ruler(1) all(4) 100 120];
-% Plot the box over the image.    
-figure('Visible','Off')
-% Enlarge figure to full screen.
-set(gcf, 'units','normalized','outerposition',[0, 0, 1, 1]);
-
-subplot(2,2,[1 3])
-text_str = ['Time: ' num2str((frame-frame_begin)/fps,'%0.2f') 's'];
-timer_img = insertText(rgbImage1,position_timer,text_str,'FontSize',100,'BoxOpacity',0.0,'TextColor','black');
-imshow(timer_img)
-% Enlarge figure to full screen.
-set(gcf, 'units','normalized','outerposition',[0, 0, 1, 1]);
-rectangle('position', boxRectruler, 'edgecolor', 'g', 'linewidth',2);
-% Give a caption above the image.
-
-%reference line
-boxRectrefline = [all(3)+crop_ruler(1) y_ref 100 1]; 
-rectangle('position', boxRectrefline, 'edgecolor', 'r', 'linewidth',1);
-
-bb(1) = bb(1)+crop_drop(1); %correcting for the drop location
-rectangle('Position', bb, 'EdgeColor', 'red');
-
-%calculate actual height and velocity
-height=ii*10.0+(bb(2)+bb(4)-boxRectrefline(2))*S1+(S2-S1)*(bb(2)+bb(4)-parallax_ref);
-if frame == frame_begin
-    velocity = 0.0; %setting the initial velocity to be zero
-    freesurf=ii*10.0+(bb(2)-boxRectrefline(2))*S1+(S2-S1)*(bb(2)-parallax_ref); %free surface location
-    %freesurf=ii*10.0+(freesurfpixel-boxRectrefline(2))*S1+(S2-S1)*(freesurfpixel-parallax_ref); %free surface location
-elseif frame > frame_begin && frame < frame_begin+10
-    velocity = (height-data(frame-frame_begin,8))/((frame-frame_begin)/fps); %for varying time frames
-else
-    velocity = (height-data(frame-frame_begin-9,8))/(10/fps); % for ten time steps
-end
-data= [data;(frame-frame_begin)/fps,ii*10.0,boxRectrefline(2), bb(1),bb(2),bb(3)*S2,bb(4)*S2,height,velocity,area_drop,height-freesurf,velocity/(bb(3)*S2)]; %Time (s), ruler reading (mm), reference pixel,bounding box - left pixel, top pixel, width, absolute height(mm), velocity (mmps),height from free surface (mm), shear rate = velocity/width (1/s) 
-
-hold on
-
-subplot(2,2,2)
-yyaxis left
-loglog(data(:,1),data(:,8),'^')
-xlim([0,(frame_end-frame_begin)/fps])
-ylim([0,600])
-ylabel('$$Height\quad(mm)$$','Interpreter','latex')
-xlabel('$$Time\quad (s)$$','Interpreter','latex')
-hold on
-yyaxis right
-loglog(data(:,1),data(:,9),'o')
- xlim([0,(frame_end-frame_begin)/fps])
-%ylim([0,200])
- ylabel('$$Velocity\quad(mm/s)$$','Interpreter','latex')
- xlabel('$$Time\quad (s)$$','Interpreter','latex')
- hold on
- 
-F(frame) = getframe(gcf) ;
-
- subplot(2,2,4)
- semilogx(data(:,1),data(:,6),'^')
-%semilogx(data(:,1),data(:,6)./data(:,7),'^')
-xlim([0,(frame_end-frame_begin)/fps])
-%ylim([0,3])
-ylabel('$$Width \quad (mm)$$','Interpreter','latex')
-xlabel('$$Time\quad (s)$$','Interpreter','latex')
-
-hold on
- 
-F(frame) = getframe(gcf) ;
-drawnow
-hold off
-
-%resetting the template sample for matching in the next loop for
-%optimization
- ii_start = all(1)-ii_thresh;
- ii_end = all(1)+ii_thresh;
- %setting the boundary of template sample for matching
- if ii_start<ii_start_thresh ii_start=ii_start_thresh; end  
- if ii_end>ii_end_thresh ii_end=ii_end_thresh; end 
- end
- 
- % create the video writer with fps of the original video
- Data_result= sprintf('%s_analyzed.avi',fffilename);
-  writerObj = VideoWriter(Data_result);
-  writerObj.FrameRate = fps; % set the seconds per image
-  open(writerObj); % open the video writer
-% write the frames to the video
-for i=frame_begin:frame_end
-    % convert the image to a frame
-    frameimg = F(i) ;
-    writeVideo(writerObj, frameimg);
-end
-% close the writer object
-close(writerObj);
-close all
-
-data_table = array2table(data,'VariableNames',{'Time_s', ...
-        'Scaleref_mm','Scalerefpixel','boundingboxleft','boundingboxtop',...
-        'Width','Height','Actualheight_mm','Velocity_mmps','Area_mm2','Heightfreesurf_mm','Shearrate_1pers'});
- 
- Data_result= sprintf('%s.mat',fffilename);
- save(Data_result,'data_table')
-%}
- 
+outputfilename = append(fffilename,'_analysed','.mat');
+save(outputfilename,'height','time');
